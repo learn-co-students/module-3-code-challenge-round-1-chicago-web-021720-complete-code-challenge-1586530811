3905e308147755f7cd8b6341d99a4e5d106b83fa
ellisandrews <ellisandrews1@gmail.com>
2020-04-10 10:36:37 -0500

    Step 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+// Global constants
+const imageId = 5099  //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  let imageId = 1 //Enter the id from the fetched image here
+// Fetch helper functions
+const parseRespJSON = response => response.json() 
+const logError = error => console.log(error) 
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+// Make a <li> node for a comment
+const makeCommentLi = comment => {
+  const li = document.createElement('li')
+  li.innerText = comment.content
+  li.dataset.id = comment.id
+  return li
+}
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+// Display an image on the page using the fetched data
+const displayImage = (image) => {
+  
+  const img = document.querySelector('#image')
+  img.src = image.url
 
-})
+  const h4 = document.querySelector('#name')
+  h4.innerText = image.name
+  
+  const likesSpan = document.querySelector('#likes')
+  likesSpan.innerText = image.like_count
+
+  const ul = document.querySelector('#comments')
+  image.comments.forEach(comment => {
+    const li = makeCommentLi(comment)
+    ul.appendChild(li)
+  })
+}
+
+// Make a fetch request to get image data
+const fetchAndDisplayImage = () => {  
+  fetch(imageURL)
+    .then(parseRespJSON)
+    .then(displayImage)
+    .catch(logError)
+}
+
+// Collect everything in a main function
+const main = () => {
+  fetchAndDisplayImage()
+}
+
+// Execute main function
+main()
